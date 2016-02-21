366e325ba095afb34d5768da4d1b47450e0d6fe9
ardeshirj <ardeshir.javaherchi@gmail.com>
2016-02-21 01:22:40 -0800

    Add the war section

--- src/main.rb
+++ src/main.rb
@@ -4,15 +4,9 @@ require_relative './player.rb'
 match = Match.new(2)
 
 until match.over?
-  puts 'Match not over yet'
+  match.players_draw_card(1)
 
-  match.player_draw_card(1)
+  match.players_draw_card(2) while match.war?
+
   match.update_winner_cards
-
-  exit
-
-  # while !match.war?(players_cards.values)
-  #   puts 'We are in the war'
-  #   break
-  # end
 end
